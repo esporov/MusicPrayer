6ade095f923df09e2e152004e5647e510bf67005
esporov <egorsporov@yandex.ru>
2023-02-12 14:50:05 +0300

    first commit

---
+++ src/main/java/ru/sporov/musicprayer/MusicPrayerApplication.java
@@ -0,0 +1,13 @@
+package ru.sporov.musicprayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPrayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPrayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sporov/musicprayer/MusicPrayerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sporov.musicprayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPrayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
